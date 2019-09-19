43568ffb7024f042e031728fbe2ce6545ed31503
Paul E. Yeager <humbl3@protonmail.com>
2019-09-18 18:12:56 -0700

    saves gend xlsx in the browser, now just need report gen data flushed out and wired to actual params

--- src/app/components/menu-form/menu-form.component.ts
+++ src/app/components/menu-form/menu-form.component.ts
@@ -4,6 +4,8 @@ import { PIMenu } from '../../models/pimenu';
 
 import { ReportGenerationService } from '../../services/report-generation.service';
 
+import { saveAs } from 'file-saver';
+
 @Component({
   selector: 'app-menu-form',
   templateUrl: './menu-form.component.html',
@@ -15,7 +17,6 @@ export class MenuFormComponent implements OnInit {
   public guidRes: string;
 
   constructor(private _reportGenerationService: ReportGenerationService) {
-
     this.model = new PIMenu("abcdefghijklmnopqr", true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true);
   }
 
@@ -23,13 +24,22 @@ export class MenuFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this._reportGenerationService.getGuid()
+    // for make_guid
+    /*this._reportGenerationService.getGuid()
       .subscribe(data => {
         this.guidRes = data.guid;
         this.submitted = true;
 
         console.log(this.guidRes);
-      });
+      });*/
+
+    // for make_report
+    this._reportGenerationService.getReport()
+      .subscribe(data => {
+        let xlsx = new Blob([data], { type: "application/xlsx" });
+        let filename = "privacy-report.xlsx";
+        saveAs(xlsx, filename);
+      })
   }
 
   // TODO: Remove this when we're done
--- src/app/services/report-generation.service.ts
+++ src/app/services/report-generation.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { GUID } from '../models/guid';
 
 @Injectable({
@@ -11,5 +11,62 @@ export class ReportGenerationService {
 
   getGuid() {
     return this.http.get<GUID>('http://localhost:3000/api/v1/make_guid');
+  }
+
+  getReport() {
+    let rawParams = {
+      "first_nameV":true,
+      "first_nameL":true,
+      "middle_nameV":false,
+      "middle_nameL":true,
+      "last_nameV":false,
+      "last_nameL":true,
+      "emailV":true,
+      "emailL":true,
+      "phoneV":false,
+      "phoneL":true,
+      "zipcodeV":false,
+      "zipcodeL":true,
+      "codeOTheDayV":false,
+      "codeOTheDayL":false,
+      "socialMediaURLV": false,
+      "socialMediaURLL": false,
+      "socialSecurityNumberV": false,
+      "socialSecurityNumberL": true,
+      "driversLicenseV": false,
+      "driversLicenseL": true,
+      "passportNumberV": false,
+      "passportNumberL": true,
+      "passportNationV": true,
+      "passportNationL": false,
+      "ageV": true,
+      "ageL": false,
+      "otherNamesUsedInThePastV": false,
+      "otherNamesUsedInThePastL": true,
+      "dxV":false,
+      "dxL":true,
+      "rxV":false,
+      "rxL":true,
+      "bloodGrpV":false,
+      "bloodGrpL":true,
+      "inTheKnowV":true,
+      "inTheKnowL":true,
+      "compliantPatientV":true,
+      "compliantPatientL":true,
+      "governmentFoodSubsistenceV": false,
+      "governmentFoodSubsistenceL": true,
+      "nonPciAcctV":true,
+      "nonPciAcctL":true,
+      "nonPciSecDigitzV":false,
+      "nonPciSecDigitzL":true
+    }
+    let params = new HttpParams();
+    Object.keys(rawParams).forEach((key, value) => {
+      params.set(key, value.toString());
+    })
+    return this.http.post('http://localhost:3000/api/v1/make_report', params, {
+      responseType: "blob",
+      headers: new HttpHeaders().append("Content-Type", "application/xlsx")
+    });
   }
 }
