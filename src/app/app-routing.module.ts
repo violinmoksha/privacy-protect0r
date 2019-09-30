327addf0c70bc9eb6bd723e2083245f9897d8c57
Paul E. Yeager <humbl3@protonmail.com>
2019-09-30 16:00:49 -0700

    push for mtg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { MenuFormComponent } from './components/menu-form/menu-form.component'
 import { DataFormComponent } from './components/data-form/data-form.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/data-form', pathMatch: 'full' },
+  { path: '', redirectTo: '/data-form/mailIDABCD1234GoesHere', pathMatch: 'full' },
   {
     path: 'menu-form',
     component: MenuFormComponent
   },
   {
-    path: 'data-form',
+    path: 'data-form/:id',
     component: DataFormComponent
   }
 ];
